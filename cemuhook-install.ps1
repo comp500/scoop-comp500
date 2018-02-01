8b5ef2b194124ae09a719d34bcdea538a8d502b1
comp500 <comp500@users.noreply.github.com>
2018-02-01 21:15:12 +0000

    Cemuhook installer finish

--- cemuhook-install.ps1
+++ cemuhook-install.ps1
@@ -3,4 +3,24 @@ $dbghelpExists = (New-Object System.IO.FileInfo("$dir\..\..\cemu\current\dbghelp
 if (!$dbghelpExists) {
 	& "$env:COMSPEC" /c mklink /h "$dir\..\..\cemu\current\dbghelp.dll" "$dir\dbghelp.dll" | out-null
 }
-#& \"$env:COMSPEC\" /c mklink /j \"$dir\\..\\..\\cemu\\current\\graphicPacks\\cemuhook\" \"$dir\\graphicPacks\" | out-null
+
+write-output "Linking keystone.dll..."
+$keystoneExists = (New-Object System.IO.FileInfo("$dir\..\..\cemu\current\keystone.dll")).Exists
+if (!$keystoneExists) {
+	& "$env:COMSPEC" /c mklink /h "$dir\..\..\cemu\current\keystone.dll" "$dir\keystone.dll" | out-null
+}
+
+write-output "Linking sample_patches.zip..."
+$samplePatchesExists = (New-Object System.IO.FileInfo("$dir\..\..\cemu\current\graphicPacks\sample_patches.zip")).Exists
+if (!$samplePatchesExists) {
+	& "$env:COMSPEC" /c mklink /h "$dir\..\..\cemu\current\graphicPacks\sample_patches.zip" "$dir\graphicPacks\sample_patches.zip" | out-null
+}
+
+write-output "Linking BreathOfTheWild_LwzxNullCheck..."
+$botwExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current\graphicPacks\BreathOfTheWild_LwzxNullCheck")).Exists
+if (!$botwExists) {
+	& "$env:COMSPEC" /c mklink /j "$dir\..\..\cemu\current\graphicPacks\BreathOfTheWild_LwzxNullCheck" "$dir\graphicPacks\BreathOfTheWild_LwzxNullCheck" | out-null
+}
+
+write-output "Copying gameProfiles..."
+Copy-Item -Path "$dir\gameProfiles" -Destination "$dir\..\..\cemu\current\gameProfiles" -Recurse
