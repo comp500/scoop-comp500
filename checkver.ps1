afee68bb3f3a9eea325b7e74071714dafc84c1f9
comp500 <comp500@users.noreply.github.com>
2018-02-01 02:12:27 +0000

    Add checkver and tasks, for updating

---
+++ checkver.ps1
@@ -0,0 +1,7 @@
+param($app) #optionally pass an app name to just check one app
+
+$bin = split-path (scoop which scoop)
+$checkver = "$bin\checkver.ps1"
+$dir = "$psscriptroot" # checks the current dir; modify if you need something else
+
+& $checkver -app $app -dir $dir -u
