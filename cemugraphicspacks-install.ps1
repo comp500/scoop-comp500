1541502ac3ccb2b283341e19f29865b782be94d9
comp500 <comp500@users.noreply.github.com>
2018-02-02 16:57:24 +0000

    Add install and uninstall magic progress thing

--- cemugraphicspacks-install.ps1
+++ cemugraphicspacks-install.ps1
@@ -1,13 +1,26 @@
 $cemuExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current")).Exists
 if ($cemuExists) {
-	Get-ChildItem $dir -Recurse -Directory |
-	ForEach-Object {
+	$directories = Get-ChildItem $dir -Recurse -Directory
+	$console = $host.UI.RawUI
+    $left = $console.CursorPosition.X
+	$top = $console.CursorPosition.Y
+	$current = 0
+
+	write-host "Linking packs... (0 / $($directories.Count))" -nonewline
+	[console]::SetCursorPosition($left, $top)
+
+	$directories | ForEach-Object {
 		$folderExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current\graphicPacks\$($_.Name)")).Exists
 		if (!$folderExists) {
-			write-output "Linking $($_.Name)"
 			& "$env:COMSPEC" /c mklink /j "$dir\..\..\cemu\current\graphicPacks\$($_.Name)" $_.FullName | out-null
 		}
+
+		$current = $current + 1
+		write-host "Linking packs... ($current / $($directories.Count))" -nonewline
+		[console]::SetCursorPosition($left, $top)
 	}
+
+	write-host "Linking packs... done.       "
 } else {
 	throw "Cannot find cemu folder. Has it been installed?"
 }
--- cemugraphicspacks-uninstall.ps1
+++ cemugraphicspacks-uninstall.ps1
@@ -1,19 +1,46 @@
 $cemuExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current")).Exists
 if ($cemuExists) {
-	Get-ChildItem $dir -Recurse -Directory |
-	ForEach-Object {
-		write-output "Unlinking $($_.Name)"
+	$directories = Get-ChildItem $dir -Recurse -Directory
+	$console = $host.UI.RawUI
+    $left = $console.CursorPosition.X
+	$top = $console.CursorPosition.Y
+	$current = 0
+
+	write-host "Unlinking packs... (0 / $($directories.Count))" -nonewline
+	[console]::SetCursorPosition($left, $top)
+
+	$directories | ForEach-Object {
+		#write-output "Unlinking $($_.Name)"
 		rm -r -Force -LiteralPath "$dir\..\..\cemu\current\graphicPacks\$($_.Name)"
+
+		$current = $current + 1
+		write-host "Unlinking packs... ($current / $($directories.Count))" -nonewline
+		[console]::SetCursorPosition($left, $top)
 	}
+
+	write-host "Unlinking packs... done."
 } else {
 	write-output "Cannot find cemu folder. If it has already been uninstalled, this is not a problem."
 	$cemuPersistExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\..\persist\cemu")).Exists
 	if ($cemuPersistExists) {
-		Get-ChildItem $dir -Recurse -Directory |
-		ForEach-Object {
-			write-output "Unlinking $($_.Name)"
+		$directories = Get-ChildItem $dir -Recurse -Directory
+		$console = $host.UI.RawUI
+		$left = $console.CursorPosition.X
+		$top = $console.CursorPosition.Y
+		$current = 0
+
+		write-host "Unlinking packs... (0 / $($directories.Count))" -nonewline
+		[console]::SetCursorPosition($left, $top)
+		$directories | ForEach-Object {
+			#write-output "Unlinking $($_.Name)"
 			rm -r -Force -LiteralPath "$dir\..\..\..\persist\cemu\graphicPacks\$($_.Name)"
+
+			$current = $current + 1
+			write-host "Unlinking packs... ($current / $($directories.Count))" -nonewline
+			[console]::SetCursorPosition($left, $top)
 		}
+
+		write-host "Unlinking packs... done.       "
 	} else {
 		write-output "Cannot find cemu persistence folder. If it has already been uninstalled, this is not a problem."
 	}
