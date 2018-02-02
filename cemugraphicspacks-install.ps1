93c58c7dfaeb6d7abd63c3e54f78aa899ef3f651
comp500 <comp500@users.noreply.github.com>
2018-02-02 16:19:51 +0000

    Test existence before creating link

--- cemugraphicspacks-install.ps1
+++ cemugraphicspacks-install.ps1
@@ -2,8 +2,11 @@ $cemuExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current")).Ex
 if ($cemuExists) {
 	Get-ChildItem $dir -Recurse -Directory |
 	ForEach-Object {
-		write-output "Linking $($_.Name)"
-		& "$env:COMSPEC" /c mklink /j "$dir\..\..\cemu\current\graphicPacks\$($_.Name)" $_.FullName | out-null
+		$folderExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\cemu\current\graphicPacks\$($_.Name)")).Exists
+		if (!$folderExists) {
+			write-output "Linking $($_.Name)"
+			& "$env:COMSPEC" /c mklink /j "$dir\..\..\cemu\current\graphicPacks\$($_.Name)" $_.FullName | out-null
+		}
 	}
 } else {
 	throw "Cannot find cemu folder. Has it been installed?"
