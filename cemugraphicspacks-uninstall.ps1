ba0e1a76af0a6ca7a328cc38a8be8f1867ca54dd
comp500 <comp500@users.noreply.github.com>
2018-02-02 16:16:50 +0000

    Fix graphicspacks tag name, and remove stuff from persistence if app removed

--- cemugraphicspacks-uninstall.ps1
+++ cemugraphicspacks-uninstall.ps1
@@ -7,4 +7,14 @@ if ($cemuExists) {
 	}
 } else {
 	write-output "Cannot find cemu folder. If it has already been uninstalled, this is not a problem."
+	$cemuPersistExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\..\persist\cemu")).Exists
+	if ($cemuPersistExists) {
+		Get-ChildItem $dir -Recurse -Directory |
+		ForEach-Object {
+			write-output "Unlinking $($_.Name)"
+			rm -r -Force "$dir\..\..\..\persist\cemu\graphicPacks\$($_.Name)"
+		}
+	} else {
+		write-output "Cannot find cemu persistence folder. If it has already been uninstalled, this is not a problem."
+	}
 }
--- cemuhook-uninstall.ps1
+++ cemuhook-uninstall.ps1
@@ -13,4 +13,14 @@ if ($cemuExists) {
 	rm -r -Force "$dir\..\..\cemu\current\graphicPacks\BreathOfTheWild_LwzxNullCheck"
 } else {
 	write-output "Cannot find cemu folder. If it has already been uninstalled, this is not a problem."
+	$cemuPersistExists = (New-Object System.IO.DirectoryInfo("$dir\..\..\..\persist\cemu")).Exists
+	if ($cemuPersistExists) {
+		write-output "Removing link to sample_patches.zip"
+		rm "$dir\..\..\..\persist\cemu\graphicPacks\sample_patches.zip"
+
+		write-output "Removing link to BreathOfTheWild_LwzxNullCheck"
+		rm -r -Force "$dir\..\..\..\persist\cemu\graphicPacks\BreathOfTheWild_LwzxNullCheck"
+	} else {
+		write-output "Cannot find cemu persistence folder. If it has already been uninstalled, this is not a problem."
+	}
 }
